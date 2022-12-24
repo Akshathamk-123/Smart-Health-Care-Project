156e26bed93c35da2add957617d7d63fadbec3f7
AishwaryaLPatil <105963603+AishwaryaLPatil@users.noreply.github.com>
2022-12-24 21:02:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,167 @@
+/*???var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors=require("cors");
+const ejs = require("ejs");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testAPI");
+var test=require('./routes/test');
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+/*app.use(bodyParser.json({limit: "30mb", extended:true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}))*/
+
+/*app.use(cors());
+const CONNECTION_URL='mongodb+srv://Aishwarya:<password>@mycluster.b2kucnm.mongodb.net/?retryWrites=true&w=majority'
+*/
+/*????
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/testAPI", testAPIRouter);
+app.use("/addproduct", test);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+/*app.listen(9000,function(){
+  console.log("Server running at 9000");
+})*/
+/*???
+app.get("/admin",function(req,res){
+  res.send("hello")
+})
+module.exports = app;*/
+
+
+
+
+/*const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Data = require("./data");
+
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+
+// this is our MongoDB database
+const dbRoute = "mongodb://127.0.0.1:27017/jelotest";
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+// this is our get method
+// this method fetches all available data in our database
+router.get("/getData", (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+// this is our update method
+// this method overwrites existing data in our database
+router.post("/updateData", (req, res) => {
+  const { id, update } = req.body;
+  Data.findOneAndUpdate(id, update, err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// this is our delete method
+// this method removes existing data in our database
+router.delete("/deleteData", (req, res) => {
+  const { id } = req.body;
+  Data.findOneAndDelete(id, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+// this is our create methid
+// this method adds new data in our database
+router.post("/putData", (req, res) => {
+  let data = new Data();
+
+  const { id, message } = req.body;
+
+  if ((!id && id !== 0) || !message) {
+    return res.json({
+      success: false,
+      error: "INVALID INPUTS"
+    });
+  }
+  data.message = message;
+  data.id = id;
+  data.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// append /api for our http requests
+app.use("/api", router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+*/
+
+const express = require("express")
+
+const app = express()
+
+app.set("view engine","ejs")
+app.set("views","./views")
+app.use(express.static("public"))
+
+
+app.get("/home",(req,res) => {
+	res.render("home")
+})
+
+app.listen(3000)
